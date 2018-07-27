2bc28ff92a92f3a0bcf4473618276963ddd94bd3
shalinjames <shalin.james@gmail.com>
2018-07-27 09:19:16 +0530

    feat(add-new-list): enable user to add new list of a board
    Includes:
    -inline-text-editor will receive label to set as input label
    -inline-text-editor will receive placeholder to set as placeholder for input
    -inline-text-editor will emit if the edit is canceled via closed property

--- src/app/board-module/list/add-list/add-list.component.ts
+++ src/app/board-module/list/add-list/add-list.component.ts
@@ -1,12 +1,33 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { Store } from "@ngxs/store";
+
+import { AddListType } from "../../../store/board.actions";
 
 @Component({
   selector: "ngx-tasker-add-list",
   templateUrl: "./add-list.component.html",
   styleUrls: ["./add-list.component.css"]
 })
-export class AddListComponent implements OnInit {
-  constructor() {}
+export class AddListComponent {
+  constructor(private store: Store) {}
+  private openTextInput: boolean = false;
+  private;
 
   ngOnInit() {}
+
+  saveNewListTitle(newTitle) {
+    this.openTextInput = !this.openTextInput;
+    this.store.dispatch(new AddListType(newTitle));
+  }
+
+  openNewListText() {
+    this.openTextInput = !this.openTextInput;
+  }
+
+  isNewListEditClosed(isClosed) {
+    console.log("isClosed", isClosed);
+    if (isClosed) {
+      this.openNewListText();
+    }
+  }
 }
--- src/app/common/inline-text-editor/inline-text-editor.component.ts
+++ src/app/common/inline-text-editor/inline-text-editor.component.ts
@@ -5,19 +5,32 @@ import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
   templateUrl: "./inline-text-editor.component.html",
   styleUrls: ["./inline-text-editor.component.css"]
 })
-export class InlineTextEditorComponent {
+export class InlineTextEditorComponent implements OnInit {
   @Input() value: string;
+  @Input() open: boolean;
+  @Input() label: string;
+  @Input() placeholder: string;
   @Output() newvalue = new EventEmitter<string>();
+  @Output() closed = new EventEmitter<boolean>();
 
-  public editTitle = false;
+  public editTitle;
   constructor() {}
 
-  toggleEditTitle() {
-    this.editTitle = !this.editTitle;
+  cancelEdit() {
+    this.closed.emit(this.editTitle);
+    this.toggleEditTitle(false);
+  }
+
+  toggleEditTitle(flag: boolean) {
+    this.editTitle = flag;
   }
 
   public onEnter(newValue) {
-    this.toggleEditTitle();
+    this.toggleEditTitle(false);
     this.newvalue.emit(newValue);
+  }
+
+  ngOnInit() {
+    this.editTitle = this.open;
   }
 }
