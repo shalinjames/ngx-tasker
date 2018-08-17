6a39dfbad2781e86473adc2121db31e1fb501c5d
shalinjames <shalin.james@gmail.com>
2018-08-17 13:20:37 +0530

    feat(add-new-card): add new card component

---
+++ src/app/board-module/add-card/add-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngxtasker-add-card',
+  templateUrl: './add-card.component.html',
+  styleUrls: ['./add-card.component.css']
+})
+export class AddCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/board-module/list/list.module.ts
+++ src/app/board-module/list/list.module.ts
@@ -7,10 +7,16 @@ import { ListMaterialModule } from "./list-material";
 import { AppCommonModule } from "../../app.common.module";
 import { AddListComponent } from "./add-list/add-list.component";
 import { CardsComponent } from "../cards/cards.component";
+import { AddCardComponent } from "../add-card/add-card.component";
 
 @NgModule({
   imports: [CommonModule, ListMaterialModule, AppCommonModule, AutofocusModule],
-  declarations: [ListComponent, AddListComponent, CardsComponent],
+  declarations: [
+    ListComponent,
+    AddListComponent,
+    CardsComponent,
+    AddCardComponent
+  ],
   exports: [ListComponent, AddListComponent, CardsComponent]
 })
 export class ListModule {}
